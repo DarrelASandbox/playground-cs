4156ff41c59083670329be71ce7febef7540ec8c
DarrelA <darrelaiscoding@gmail.com>
2022-11-13 01:24:22 +0800

    06-Methods: CalculateTax()

---
+++ harsha-vardhan/06-Methods/Product.cs
@@ -0,0 +1,16 @@
+﻿public class Product
+{
+  public double cost;
+  public double tax;
+
+  public void CalculateTax()
+  {
+    double t;
+
+    if (cost <= 20000) t = cost * 10 / 100;
+    else t = cost * 12.5 / 100;
+
+    tax = t;
+  }
+}
+
---
+++ harsha-vardhan/06-Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_Methods
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Product p1, p2;
+      p1 = new Product();
+      p2 = new Product();
+
+      p1.cost = 1000;
+      p2.cost = 30_000;
+
+      p1.CalculateTax();
+      p2.CalculateTax();
+
+      Console.WriteLine("-------------------- p1 --------------------:");
+      Console.WriteLine("cost: " + p1.cost);
+      Console.WriteLine("CalculateTax: " + p1.tax);
+
+      Console.WriteLine("\n-------------------- p2 --------------------:");
+      Console.WriteLine("cost: " + p2.cost);
+      Console.WriteLine("CalculateTax: " + p2.tax);
+    }
+  }
+}
